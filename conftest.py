8ce555bfd37fcb1161dcf9b8ee0d9584d705f306
Rich Tier <rikatee@gmail.com>
2016-11-23 21:42:09 +0000

    Add company case study validators

--- conftest.py
+++ conftest.py
@@ -3,4 +3,6 @@ def pytest_configure():
     settings.configure(
         DEBUG_PROPAGATE_EXCEPTIONS=True,
         VALIDATOR_MAX_LOGO_SIZE_BYTES=10 * 1024 * 1024,
+        VALIDATOR_MAX_CASE_STUDY_IMAGE_SIZE_BYTES=2 * 1024 * 1024,
+        VALIDATOR_MAX_CASE_STUDY_VIDEO_SIZE_BYTES=50 * 1024 * 1024,
     )
