545c488aa383fb0db4e1a6afa263760f6baee590
Alessandro De Noia <alessandro.denoia@digital.trade.gov.uk>
2017-11-28 11:43:25 +0000

    Add password word validator

--- directory_validators/password_validation.py
+++ directory_validators/password_validation.py
@@ -1,7 +1,10 @@
+import re
 from django.core.exceptions import ValidationError
 
 ALPHABETIC_VALIDATION_MESSAGE = 'This password contains letters only.'
 ALPHABETIC_HELP_TEXT = 'Your password cannot contain letters only.'
+PASSWORD_WORD_VALIDATION_MESSAGE = 'This password contain the word password'
+PASSWORD_WORD_HELP_TEXT = 'Your password cannot contain the word password'
 
 
 class AlphabeticPasswordValidator:
@@ -15,3 +18,16 @@ class AlphabeticPasswordValidator:
 
     def get_help_text(self):
         return ALPHABETIC_HELP_TEXT
+
+
+class PasswordWordPasswordValidator:
+
+    def validate(self, password, user=None):
+        if re.search('password', password, re.IGNORECASE):
+            raise ValidationError(
+                PASSWORD_WORD_VALIDATION_MESSAGE,
+                code='password_used_in_password'
+            )
+
+    def get_help_text(self):
+        return PASSWORD_WORD_HELP_TEXT
--- directory_validators/tests/test_password_validator.py
+++ directory_validators/tests/test_password_validator.py
@@ -24,3 +24,33 @@ def test_alphabetic_password_validator_exception_raised(password):
     with pytest.raises(ValidationError) as e:
         validator.validate(password=password)
     assert password_validation.ALPHABETIC_VALIDATION_MESSAGE in str(e.value)
+
+
+@pytest.mark.parametrize(
+    'password',
+    ('foobar', '12ab', '123', '%£$K')
+)
+def test_common_word_password_validator(password):
+    validator = password_validation.PasswordWordPasswordValidator()
+    assert validator.validate(password=password) is None
+    assert validator.get_help_text() == \
+        password_validation.PASSWORD_WORD_HELP_TEXT
+
+
+@pytest.mark.parametrize(
+    'password',
+    (
+        'password',
+        'PASSWORD',
+        'PaSsWord',
+        'FooPasswordBar',
+        'foobarpassWord',
+        'barPASSWORD',
+        'passwordpassword'
+    )
+)
+def test_common_word_validator_exception_raised(password):
+    validator = password_validation.PasswordWordPasswordValidator()
+    with pytest.raises(ValidationError) as e:
+        validator.validate(password=password)
+    assert password_validation.PASSWORD_WORD_VALIDATION_MESSAGE in str(e.value)
