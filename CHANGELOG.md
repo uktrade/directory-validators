23006b5d93ba711175feb96e2d9ad1633c307905
richtier <rikatee@gmail.com>
2019-05-29 10:00:33 +0100

    Fix security vulnerability

---
+++ CHANGELOG.md
@@ -0,0 +1,12 @@
+# Changelog
+
+## [5.1.1](https://pypi.org/project/directory-validators/5.1.1/) (2019-05-29)
+[Full Changelog](https://github.com/uktrade/directory-validators/pull/55/files)
+
+### Implemented enhancements
+- no ticket - PyPI now renders the markdown readme
+
+### Fixed bugs:
+- Upgraded minimum supported django to avoid security vulnerability 
+- Upgraded minimum supported urllib3 to avoid security vulnerability 
+
--- setup.py
+++ setup.py
@@ -1,32 +1,12 @@
 """
 Export Directory API client
 """
-import ast
-import pip.download
-from pip.req import parse_requirements
-import re
 from setuptools import setup, find_packages
-
-
-def get_version():
-    pattern = re.compile(r'__version__\s+=\s+(.*)')
-
-    with open('directory_validators/version.py', 'rb') as src:
-        return str(ast.literal_eval(
-            pattern.search(src.read().decode('utf-8')).group(1)
-        ))
-
-
-def get_requirements():
-    return [str(r.req) for r in list(parse_requirements(
-        'requirements.txt',
-        session=pip.download.PipSession()
-    ))]
 
 
 setup(
     name='directory_validators',
-    version=get_version(),
+    version='5.1.1',
     url='https://github.com/uktrade/directory-validators',
     license='MIT',
     author='Department for International Trade',
@@ -34,7 +14,26 @@ setup(
     packages=find_packages(exclude=["tests.*", "tests"]),
     long_description=open('README.md').read(),
     include_package_data=True,
-    install_requires=get_requirements(),
+    install_requires=[
+        'django>=1.11.20,<2.0a1',
+        'olefile==0.44',
+        'phonenumberslite==7.7.4',
+        'pillow==4.2.1',
+        'pytz==2017.2',
+        'urllib3>=1.24.2<2.0.0',
+    ],
+    extras_require={
+        'test': [
+            'pytest==3.0.2',
+            'pytest-cov==2.3.1',
+            'pytest-sugar==0.8.0',
+            'flake8==3.0.4',
+            'codecov==2.0.9',
+            'twine>=1.11.0,<2.0.0',
+            'wheel>=0.31.0,<1.0.0',
+            'setuptools>=38.6.0,<39.0.0',
+        ]
+    },
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
@@ -45,6 +44,7 @@ setup(
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ]
 )
